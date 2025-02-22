1016e1d6d510a8e157cae5f244a00eaa698eabe4
Arne Hassel <arne.hassel@gmail.com>
2025-02-22 19:15:59 +0100

    Handling multiple response formats

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import uvicorn
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from starlette.requests import Request
 from starlette.responses import FileResponse
 
 app = FastAPI()
 
-@app.get("/", response_class=HTMLResponse)
-def read_root():
+
+@app.get("/")
+def read_norwegian(request: Request):
+    if request.headers.get("Accept") == "text/turtle":
+        return FileResponse(".build/data.ttl", media_type="text/turtle")
     return FileResponse(".build/no.html")
 
-@app.get("/en", response_class=HTMLResponse)
-def read_item():
+
+@app.get("/en")
+def read_english(request: Request):
+    if request.headers.get("Accept") == "text/turtle":
+        return FileResponse(".build/data.ttl")
     return FileResponse(".build/en.html")
 
+
 if __name__ == "__main__":
-    uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
