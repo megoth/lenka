77682359a3e579aa1f056c017f6bd0547f6f56e8
Arne Hassel <arne.hassel@gmail.com>
2025-02-22 23:47:34 +0100

    Improved output slightly

--- build.py
+++ build.py
@@ -5,4 +5,10 @@ g.parse('src/data/communities.ttl')
 g.parse('src/data/courses.ttl')
 g.serialize(destination='.build/data.ttl', format='turtle')
 g.serialize(destination='.build/data.xml', format='xml')
-g.serialize(destination='.build/data.json', format='json-ld', auto_compact=True)
+g.serialize(destination='.build/data.json', format='json-ld', context={
+    "lenka": "https://lenka.no/#",
+    "schema": "https://schema.org/",
+    "description": "schema:description",
+    "name": "schema:name",
+    "url": "schema:url",
+})
