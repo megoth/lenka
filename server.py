55878ef9a306c5bcb586709d09648627777ba31a
Arne Hassel <arne.hassel@gmail.com>
2025-02-22 16:28:10 +0100

    More config with Vercel

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from starlette.responses import FileResponse
@@ -11,3 +12,6 @@ def read_root():
 @app.get("/en", response_class=HTMLResponse)
 def read_item():
     return FileResponse(".build/en.html")
+
+if __name__ == "__main__":
+    uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
