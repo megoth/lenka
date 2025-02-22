88cc33720764ba3e4244b0f7685b5f363db812e5
Arne Hassel <arne.hassel@gmail.com>
2025-02-23 00:10:26 +0100

    Extended build-script to build HTML-file
    Will start adding HTML+RDFa and styling next

--- build.py
+++ build.py
@@ -3,6 +3,7 @@ from rdflib import Graph
 g = Graph()
 g.parse('src/data/communities.ttl')
 g.parse('src/data/courses.ttl')
+
 g.serialize(destination='.build/data.ttl', format='turtle')
 g.serialize(destination='.build/data.xml', format='xml')
 g.serialize(destination='.build/data.json', format='json-ld', context={
@@ -12,3 +13,20 @@ g.serialize(destination='.build/data.json', format='json-ld', context={
     "name": "schema:name",
     "url": "schema:url",
 })
+
+from jinja2 import Environment, select_autoescape, FileSystemLoader
+
+env = Environment(
+    loader=FileSystemLoader(searchpath="src/templates"),
+    autoescape=select_autoescape()
+)
+
+
+def serialize_html(lang: str, body: str):
+    template = env.get_template("index.html")
+    with open(f".build/{lang}.html", "w") as f:
+        f.write(template.render(lang=lang, body=body))
+
+
+serialize_html("en", "Hello World!")
+serialize_html("no", "Hei verden!")
