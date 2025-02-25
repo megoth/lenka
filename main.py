8fcbbf18b9b393049d0da666287061e052d31a11
Arne Hassel <arne.hassel@gmail.com>
2025-02-25 14:34:12 +0100

    Serving memory-cached instances instead of file-cached

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 import uvicorn
 from fastapi import FastAPI
 from starlette.requests import Request
-from starlette.responses import FileResponse
+from starlette.responses import FileResponse, PlainTextResponse
+from rdflib import Graph
 
 app = FastAPI()
+
+g = Graph()
+g.parse('src/data/communities.ttl')
+g.parse('src/data/courses.ttl')
+
+jsonld = g.serialize(format='json-ld',
+                     context={"lenka": "https://lenka.no/#", "schema": "https://schema.org/",
+                              "description": "schema:description", "name": "schema:name",
+                              "url": "schema:url", })
+rdfxml = g.serialize(format='xml')
+turtle = g.serialize(format='turtle')
 
 
 def read_response(request: Request, html_file: str):
     if request.headers.get("Accept") == "application/ld+json":
-        return FileResponse(".build/data.json", media_type="application/ld+json")
+        return PlainTextResponse(jsonld, media_type="application/ld+json")
     if request.headers.get("Accept") == "application/rdf+xml":
-        return FileResponse(".build/data.xml", media_type="application/rdf+xml")
+        return PlainTextResponse(rdfxml, media_type="application/rdf+xml")
     if request.headers.get("Accept") == "text/turtle":
-        return FileResponse(".build/data.ttl", media_type="text/turtle")
+        return PlainTextResponse(turtle, media_type="text/turtle")
     return FileResponse(html_file)
 
 
